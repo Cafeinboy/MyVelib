5c2abb8b919ca5dc6ddceb69a5e72c622871b83b
Malo Cayocca <malo.cayocca@student.ecp.fr>
2018-04-08 22:09:56 +0200

    Tout vert

--- MyVelibGitHub/src/User/User.java
+++ MyVelibGitHub/src/User/User.java
@@ -148,6 +148,7 @@ public class User implements Observer, Functionnality {
 			System.out.println(message);
 			Ride oldRide = this.getRide();
 			Ride newRide = new Ride(oldRide.getListStation().get(0).getCoordinates(), oldRide.getFinishingPoint(), oldRide.getRideStrategy(), oldRide.getListStation().get(1).knowHisNetwork());
+			newRide.setWishBike(oldRide.getWishBike());
 			newRide.haveARide(oldRide.getWishBike());
 			this.getRide().setListStation(new ArrayList<Station>(Arrays.asList(oldRide.getListStation().get(0), newRide.getListStation().get(1))));
 			System.out.println("Your new destination station is " + this.getRide().getListStation().get(1).getName());
