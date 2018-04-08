576bcc5612e0cc2f8e9a13f2ffd98f46f0743984
Malo Cayocca <malo.cayocca@student.ecp.fr>
2018-04-08 10:49:11 +0200

    Fin des scenarios

--- MyVelibGitHub/src/User/User.java
+++ MyVelibGitHub/src/User/User.java
@@ -145,7 +145,6 @@ public class User implements Observer, Functionnality {
 			String message = (String) arg;
 			System.out.println(message);
 			Ride oldRide = this.getRide();
-			System.out.println(oldRide);
 			Ride newRide = new Ride(oldRide.getListStation().get(0).getCoordinates(), oldRide.getFinishingPoint(), oldRide.getRideStrategy(), oldRide.getListStation().get(1).knowHisNetwork());
 			newRide.haveARide(oldRide.getWishBike());
 			this.getRide().setListStation(new ArrayList<Station>(Arrays.asList(oldRide.getListStation().get(0), newRide.getListStation().get(1))));
